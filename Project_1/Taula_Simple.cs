3d3aed5180395a74b8d93e511c9877b56da56ffa
edubastida <eduardbastidaperez@gmail.com>
2020-11-02 17:25:57 +0100

    j

---
+++ Project_1/Taula_Simple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1
+{
+    public partial class Taula_Simple : TLR_Forms.BaseForm
+    {
+        public Taula_Simple()
+        {
+            InitializeComponent();
+        }
+
+        private void Taula_Simple_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
