ce43989c38d1f4fdbd9739fa3c47fd37eebc6e5a
edubastida <eduardbastidaperez@gmail.com>
2020-11-02 18:12:53 +0100

    02/11/2020

--- Project_1/Log_in.cs
+++ Project_1/Log_in.cs
@@ -27,7 +27,7 @@ namespace Project_1
                 textBox1.Enabled = false;
                 textBox2.Enabled = false;
                 label6.Text = ("Bon dia " + textBox1.Text + ", estem validant les seves credencials");
-                pictureBox1.Image = Image.FromFile(@"C:\Users\rprod\Pictures\conejo_cola.gif");
+                //pictureBox1.Image = Image.FromFile(@"C:\Users\rprod\Pictures\conejo_cola.gif");
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
                 timer1.Enabled = true;
                 timer1.Start();      
