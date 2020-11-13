6d643f925a8082b93eaa3b96914fff3f936c03ad
JavierCopete <javier.copete@sarria.salesians.catS>
2020-11-13 16:18:15 +0100

    13/11/20

---
+++ TLR_controls/Boton_Min.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.IsolatedStorage;
+
+namespace Tlr_controls
+{
+    public partial class Boton_Min : Button
+    {
+        public Boton_Min()
+        {
+            InitializeComponent();
+        }
+
+        public Boton_Min(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        private void Boton_Min_Click(object sender, EventArgs e)
+        {
+            Form frm = this.FindForm();
+            frm.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
