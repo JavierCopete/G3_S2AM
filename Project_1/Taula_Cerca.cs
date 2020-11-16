d49bdb11e89e26c8f9c274b9e14b5df25bd73072
JavierCopete <javier.copete@sarria.salesians.catS>
2020-11-16 16:51:55 +0100

    16/11/20

---
+++ Project_1/Taula_Cerca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1
+{
+    public partial class Taula_Cerca : TLR_Forms.BaseForm
+    {
+        public Taula_Cerca()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project_1/Taula_foranea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1
+{
+    public partial class Taula_Foranea : TLR_Forms.BaseTaulaSimple
+    {
+        public Taula_Foranea()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
