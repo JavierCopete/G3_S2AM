6a05b08050f5c994b43a5dd5db3bf5bff8a06174
edubastida <eduardbastidaperez@gmail.com>
2020-11-26 17:05:05 +0100

    26/11/2020

--- TLR_Dades/Dades.cs
+++ TLR_Dades/Dades.cs
@@ -15,9 +15,9 @@ namespace TLR_Dades
 
         private void Connectar()
         {
-            //cnx = "Data Source=JAVIERC;Initial Catalog=SecureCore;Integrated Security=True";
-            //cnx = "Data Source = LAPTOP - 3CMMKCMH\\SQLEXPRESS; Initial Catalog = SecureCore; Integrated Security = True";
-            cnx = "Data Source=MSI-EDU\\SQLExpress;Initial Catalog=SecureCore;Integrated Security=True";
+            //cnx = "Data Source=DESKTOP-NL97QBH;Initial Catalog=SecureCore;Integrated Security=True"; //Javier
+            //cnx = "Data Source=LAPTOP-3CMMKCMH;Initial Catalog=SecureCore;Integrated Security=True"; //Rubén
+            //cnx = "Data Source=MSI-EDU\\SQLExpress;Initial Catalog=SecureCore;Integrated Security=True"; //Edu
             conn = new SqlConnection(cnx);
         }
 
